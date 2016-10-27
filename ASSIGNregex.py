9710ba2a075eb1beaa83d5b47c9288521907e666
Josh Navarro <josh@AVC-MBP-SB-2.local>
2016-10-27 17:28:42 -0400

    this is a test

---
+++ ASSIGNregex.py
@@ -0,0 +1,9 @@
+import re
+
+fhand = open('ACTUALregex.txt')
+numlist = list()
+numtextlist = re.findall('[0-9]+', fhand.read())
+for num in numtextlist:
+	numint = int(num)
+	numlist.append(numint)
+print sum(numlist)
